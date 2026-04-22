b585af7049828297ba3d2c9b32507b3f47613b73
LacraiaVermelha <projmiguel@outlook.com>
2026-04-22 20:33:12 +0000

    Aula_3 feita

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,54 @@
 import Foundation
 
-print("Aula 3 de Swift!!")
+class cofre {
+    private var id:Int
+    private var nome:String
+    private var saldo:Double
+    private var limite:Double
+
+    init(_ nome:String,_  id:Int) {
+        self.id = id
+        self.nome = nome
+        self.saldo = 0.0
+        self.limite = 0.0
+    }
+
+    public func depositar(_ quantia: Double) {
+        if (quantia > 0) {
+            saldo += quantia
+        }
+        else {
+            print("ERRO. Valor inválido.")
+        }
+    }
+
+    public func sacar(_ quantia: Double) {
+        if (quantia <= 0) {
+            print("ERRO. Valor inválido.")
+        }
+        else if (saldo-quantia < limite) {
+            print("ERRO. Saldo insuficiente.")
+        }
+        else {
+            saldo -= quantia
+        }
+    }
+
+    public func setLimite(_ novoLimite: Double) {
+        if (novoLimite > 0) {
+            print("ERRO. Limite de crédito deve ser menor que zero.")
+        }
+        else {
+            limite = novoLimite
+        }
+    }
+
+    public func getSaldo() -> Double {
+        return saldo
+    }
+}
+
+var conta = cofre.init("CARLOS", 01)
+conta.setLimite(-10)
+conta.sacar(10)
+print(conta.getSaldo())
