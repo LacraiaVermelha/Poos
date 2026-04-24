9ae664d82eb5b45376ff01f80ea416f908f5e4b8
LacraiaVermelha <projmiguel@outlook.com>
2026-04-24 20:17:05 +0000

    aula_05

--- Sources/Aula_5/main.swift
+++ Sources/Aula_5/main.swift
@@ -1,3 +1,33 @@
 import Foundation
 
-print("Aula 5 de Swift!!")
+class user {
+    var nome: String
+    var email: String
+
+    init(_ nome:String, _ email:String) {
+        self.nome = nome
+        self.email = email
+    }
+
+    func getDescription() {
+        print("Usuário: \(nome), email: \(email)")
+    }
+}
+
+class aluno: user {
+    let matricula:Int
+    let pagamento:tipoPagamento
+
+    public enum tipoPagamento {
+        case mensal, anual
+    }
+
+    init(_ nome:String, _ email:String, _ matricula:Int, _ pagamento:tipoPagamento) {
+        self.matricula = matricula
+        self.pagamento = pagamento
+        super.init(nome, email)
+    }
+}
+
+let joao = user("Jorge", "jorgeemail@email.com")
+let carlos = user("Carlos", "carlosemail@email.com")
